272d3dca48933a782b04b8f490fc882ff875c6cf
AlexTheHomebody <catrinaaa1230@gmail.com>
2023-01-04 17:01:19 -0600

    first nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import "./main.css";
+import NavBar from "./Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App">JAM JET LESS GOOOOo</div>;
+  return <div>
+            <NavBar />
+            <h1 className="App">JAM JET LESS GOOOOo</h1>
+          </div>;
 }
 
 export default App;
