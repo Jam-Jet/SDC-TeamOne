5cfed4c688c3f003c2590e746d2e4fc323fc0523
AlexTheHomebody <catrinaaa1230@gmail.com>
2023-01-05 12:13:45 -0600

    start login

--- client/src/components/live-chat-page/Login.jsx
+++ client/src/components/live-chat-page/Login.jsx
@@ -3,6 +3,12 @@ const Login = () => {
       <div>
         <main>
           <div id="login-main">
+            <div className="login-title-total">
+                <div className="login-title">USER LOGIN</div>
+                <div className="login-welcome">Welcome to the WorldWideChat</div>
+            </div>
+            <input type="text" />
+            <input type="text" />
             
           </div>
         </main>
@@ -12,3 +18,21 @@ const Login = () => {
   
   export default Login;
   
+
+//   <div id="chat-wrapper">
+//           <div id="chat-content">
+//             <RecipientChatBlurb />
+//             <UserChatBlurb />
+//             <RecipientChatBlurb />
+//             <UserChatBlurb />
+//             <RecipientChatBlurb />
+//             <UserChatBlurb />
+//             <RecipientChatBlurb />
+//             <UserChatBlurb />
+//             <RecipientChatBlurb />
+//             <UserChatBlurb />
+//             <RecipientChatBlurb />
+//             <UserChatBlurb />
+//             <RecipientChatBlurb />
+//             <UserChatBlurb />
+//           </div>
